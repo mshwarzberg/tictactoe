f99106c4b0080c6d3d69b66c18727fdcbb960140
Mendel Shwarzberg <mshwarzberg@gmail.com>
2022-09-12 17:40:11 -0400

    Made a tic tac toe game

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import Box from "./Box";
+
+function App() {
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState("X");
+  const [winner, setWinner] = useState();
+
+  useEffect(() => {
+    const winningPositions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+    ];
+
+    for (const positions of winningPositions) {
+      const [a, b, c] = positions;
+      if (!board[a] || !board[b] || !board[c]) {
+        continue;
+      }
+      if (board[a] === board[b] && board[b] === board[c]) {
+        setWinner(board[a]);
+        break;
+      }
+    }
+  }, [player, board]);
+
+  useEffect(() => {
+    if (winner) {
+      alert(winner);
+    }
+  }, [winner]);
+
+  const renderBoard = board.map((item, index) => {
+    return (
+      <Box
+        value={item}
+        key={index}
+        setBoard={setBoard}
+        index={index}
+        player={player}
+        setPlayer={setPlayer}
+        winner={winner}
+      />
+    );
+  });
+
+  return (
+    <>
+      <h1>Tic Tac Toe</h1>
+      {winner ? <h1>{winner} has won!</h1> : <h1>{player}'s turn</h1>}
+      <div id="grid-container">{renderBoard}</div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Box.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function Box({
+  value,
+  setBoard,
+  index,
+  player,
+  setPlayer,
+  winner,
+}) {
+  return (
+    <div
+      className="box"
+      onClick={() => {
+        if (value || winner) {
+          return;
+        }
+        setBoard((prevBoard) =>
+          prevBoard.map((prevItem, prevIndex) => {
+            if (prevIndex === index) {
+              return player;
+            }
+            return prevItem;
+          })
+        );
+        if (player === "X") {
+          setPlayer("O");
+        } else {
+          setPlayer("X");
+        }
+      }}
+    >
+      {value}
+    </div>
+  );
+}
